64cbfd6a46862b75a4669dabb0c7ef7c22539e91
Kah Wai LIM <kahwai222@gmail.com>
2021-03-12 23:57:19 +0800

    update k8s manifests

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,7 +5,8 @@ const buildClient = ({ req }) => {
     // We are on the server
 
     return axios.create({
-      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      // baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://rna.services/',
       headers: req.headers,
     });
   } else {
